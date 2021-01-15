e0a05919ee330e8214bed845e7e691edb3c7f4eb
Panchober27 <58098908+Panchober27@users.noreply.github.com>
2021-01-15 18:12:00 -0300

    XXX

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,17 @@
 const { Router } = require('express');
 const router = Router();
 
+// Ruta principal del servidor.
 router.get('/', (req, res) => {
   res.render('index');
 })
+
+
+
+// Otras rutas...
+
+
+
+
 
 module.exports = router;
